ce3626c41ca741d1db58c68bf842188f9c36cffb
Yudana Satria Kinanto <yudanasatria@gmail.com>
2022-11-25 10:26:58 +0700

    Add Middelware|Token

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\Http\Controllers\PostController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use Illuminate\Auth\Middleware\Authenticate;
+use App\Http\Controllers\AuthenticationController;
 
 
 
@@ -9,3 +12,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/posts',[PostController::class,'index'])->middleware(['auth:sanctum']);
 Route::get('/posts/{id}',[PostController::class,'show'])->middleware(['auth:sanctum']);
 Route::get('/posts2/{id}',[PostController::class,'show2']);
+
+Route::post('/login', [AuthenticationController::class,'login']);
