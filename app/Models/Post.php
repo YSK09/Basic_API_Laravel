be3b819a5c6be89e1e91708b15cfd23f0deb9567
Yudana Satria Kinanto <yudanasatria@gmail.com>
2022-11-25 09:16:26 +0700

    Add route|controller|Resoures|eager Loading

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use PhpParser\Node\Stmt\Return_;
 
 class Post extends Model
 {
@@ -11,4 +13,9 @@ class Post extends Model
     protected $fillable = [
         'title','news_content','author'
     ];
+
+    public function writer():BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author', 'id');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::post('/posts',[PostController::class,'index']);
+Route::get('/posts',[PostController::class,'index']);
+Route::get('/posts/{id}',[PostController::class,'show']);
+Route::get('/posts2/{id}',[PostController::class,'show2']);
